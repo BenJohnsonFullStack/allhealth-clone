ac3ac0f68813794eb93220ab7f7adcc2e1c5b4d5
Ben Johnson <monk.sound.design@gmail.com>
2023-11-04 10:29:12 -0400

    hero image slider data

---
+++ constants/index.js
@@ -0,0 +1,34 @@
+const heroImageSlider = [
+  {
+    id: 1,
+    url: "",
+    title: "24/7 Population Health Management Team",
+  },
+  {
+    id: 2,
+    url: "",
+    title: "Levels of Care for the Right Level of Risk",
+  },
+  {
+    id: 3,
+    url: "",
+    title: "Health Risk Assessment",
+  },
+  {
+    id: 4,
+    url: "",
+    title: "Lower Cost, Improved Outcomes",
+  },
+  {
+    id: 5,
+    url: "",
+    title: "MyCharlie Easy to Use App",
+  },
+  {
+    id: 6,
+    url: "",
+    title: "Digital to Human Experience",
+  },
+];
+
+export { heroImageSlider };
