188ce6e711425ffdd60f904dec42cf4729d02983
Ben Johnson <monk.sound.design@gmail.com>
2023-11-04 03:18:56 -0400

    responsive styles

--- app/page.jsx
+++ app/page.jsx
@@ -99,84 +99,91 @@ export default function Home() {
       </section> */}
 
       {/* home page main content */}
-      <main className="w-[min(100%-4em,75ch)] bg-white mx-auto">
-        <div className="mt-20 mx-auto">
-          <div className="w-full">
-            <Image
-              src="https://images.unsplash.com/photo-1576091160399-112ba8d25d1d?auto=format&fit=crop&q=80&w=870&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt="an image of a doctor holding a cellphone analyzing data"
-              width={1920}
-              height={1280}
-              className="rounded-md shadow-md"
-            />
-          </div>
-          <p className="text-[1.2rem] py-6 text-gray-800">
-            AllHealth CHOICE is a data driven population health management
-            company that evokes change behaviors and improves health outcomes
-            resulting in overall reduced healthcare expenses.
-          </p>
-
-          <div className="w-full mt-12">
-            <Image
-              src="https://allhealthchoice.com/wp-content/uploads/2023/05/membs.jpg"
-              alt=" several images of the All Health Choice MyCharlie app displayed on cell phones"
-              width={500}
-              height={234}
-            />
+      <main className="w-[min(100%-4em,70%)] bg-white mx-auto">
+        <div className="mt-20">
+          <div className="flex flex-col justify-center items-center md:flex-row md:gap-10">
+            <div className="w-full sm:w-1/2 mx-auto">
+              <Image
+                src="https://images.unsplash.com/photo-1576091160399-112ba8d25d1d?auto=format&fit=crop&q=80&w=870&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="an image of a doctor holding a cellphone analyzing data"
+                width={1920}
+                height={1280}
+                className="rounded-md shadow-md block mx-auto"
+              />
+            </div>
+            <p className="text-[1.2rem] md:text-[1.8rem] py-4 my-12 text-gray-800 md:max-w-[40%]">
+              AllHealth CHOICE is a data driven population health management
+              company that evokes change behaviors and improves health outcomes
+              resulting in overall reduced healthcare expenses.
+            </p>
           </div>
 
-          <div>
-            <p className="text-[1.2rem] py-6 text-gray-800">
-              AllHealth CHOICE (AHC) values innovation and understands the
-              benefits that a data centric approach can bring to the care
-              management of total populations.{" "}
-            </p>
+          <div className="flex flex-col justify-center items-center md:flex-row-reverse md:gap-10">
+            <div className="w-full">
+              <Image
+                src="https://allhealthchoice.com/wp-content/uploads/2023/05/membs.jpg"
+                alt=" several images of the All Health Choice MyCharlie app displayed on cell phones"
+                width={500}
+                height={234}
+                className="block mx-auto"
+              />
+            </div>
 
-            <ul className="list-disc px-8 text-gray-800">
-              <li className="py-1">
-                Bringing a human touch to digital solutions
-              </li>
-              <li className="py-1">
-                The latest in healthcare claims and pharmacy analytics
-              </li>
-              <li className="py-1">
-                Mobile app technology and remote monitoring medical devices
-              </li>
-              <li className="py-1">24/7 health coach coordination</li>
-            </ul>
+            <div className="md:max-w-[40%]">
+              <p className="text-[1.2rem] mt-12 py-6 text-gray-800">
+                AllHealth CHOICE (AHC) values innovation and understands the
+                benefits that a data centric approach can bring to the care
+                management of total populations.{" "}
+              </p>
 
-            <p className="py-6 text-[1.2rem] text-gray-800">
-              We believe in the power of our technology to improve outcomes,
-              boost member productivity, and reduce overall claims spend.
-            </p>
+              <ul className="list-disc px-8 text-gray-800">
+                <li className="py-1">
+                  Bringing a human touch to digital solutions
+                </li>
+                <li className="py-1">
+                  The latest in healthcare claims and pharmacy analytics
+                </li>
+                <li className="py-1">
+                  Mobile app technology and remote monitoring medical devices
+                </li>
+                <li className="py-1">24/7 health coach coordination</li>
+              </ul>
+
+              <p className="py-6 text-[1.2rem] text-gray-800">
+                We believe in the power of our technology to improve outcomes,
+                boost member productivity, and reduce overall claims spend.
+              </p>
+            </div>
           </div>
         </div>
       </main>
 
       <section>
         <footer className="bg-[#1b75bc] mt-12 text-center text-[#e6f0f8]">
-          <div className="py-12">
+          <div className="py-12 flex flex-col justify-center items-center md:flex-row md:justify-around">
             <div>
-              <Image
-                src="https://allhealthchoice.com/wp-content/uploads/2021/06/AllHealth.wewhiteb-small.png"
-                alt="all-health choice logo in white"
-                width={300}
-                height={75}
-                className="block mx-auto mr-8"
-              />
-            </div>
+              <div>
+                <Image
+                  src="https://allhealthchoice.com/wp-content/uploads/2021/06/AllHealth.wewhiteb-small.png"
+                  alt="all-health choice logo in white"
+                  width={300}
+                  height={75}
+                  className="block mx-auto"
+                />
+              </div>
 
-            <address className="not-italic mt-10 ml-3 leading-loose">
-              <a href="email:info@allhealthchoice.com">
-                info@allhealthchoice.com
-              </a>
-              <p>
-                Office: <a href="tel:4049960086">404.996.0086</a>
-              </p>
-              <p>
-                Toll Free: <a href="tel:8557130255">855.713.0255</a>
-              </p>
-            </address>
+              <address className="not-italic mt-10 ml-3 leading-loose">
+                <a href="email:info@allhealthchoice.com">
+                  info@allhealthchoice.com
+                </a>
+                <p>
+                  Office: <a href="tel:4049960086">404.996.0086</a>
+                </p>
+                <p>
+                  Toll Free: <a href="tel:8557130255">855.713.0255</a>
+                </p>
+              </address>
+            </div>
 
             <div className="mt-10 text-lg flex flex-col leading-loose">
               <a href="https://allhealthchoice.com/privacy-statement/">
