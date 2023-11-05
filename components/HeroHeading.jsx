3b7f4e1987c81af2153ebde45650a0e62f1c9293
Ben Johnson <monk.sound.design@gmail.com>
2023-11-05 09:52:26 -0500

    adjust heading width for mobile

--- components/HeroHeading.jsx
+++ components/HeroHeading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HeroHeading = ({ title }) => {
   return (
-    <div className="w-[65%] lg:w-[45%] pb-12 md:pb-20 lg:pb-32 pt-24 lg:pt-40 mx-auto text-left text-white relative">
+    <div className="w-[60%] lg:w-[45%] pb-12 md:pb-20 lg:pb-32 pt-24 lg:pt-40 mx-auto text-left text-white relative">
       <h1 className="text-xl md:text-[1.6rem] lg:text-[2rem] font-bold">
         {title}
       </h1>
