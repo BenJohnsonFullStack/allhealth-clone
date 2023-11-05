ca9d3b22ac992115a09873d7ecc988dcab860ef3
Ben Johnson <monk.sound.design@gmail.com>
2023-11-05 01:55:16 -0500

    hero font sizes

--- components/HeroContent.jsx
+++ components/HeroContent.jsx
@@ -4,7 +4,7 @@ const HeroContent = () => {
   return (
     <>
       <div id="hero-content-conatiner">
-        <div className="w-48 hover:w-[12.2rem] cursor-pointer transition-all duration-200 lg:w-60 absolute top-[48px] left-[0px] z-[99]">
+        <div className="w-48 cursor-pointer transition-all duration-200 lg:w-60 absolute top-[48px] left-[0px] z-[99]">
           <Image
             src="https://allhealthchoice.com/wp-content/uploads/2021/06/newlogo.png"
             alt="all-health choice logo in color"
--- components/HeroHeading.jsx
+++ components/HeroHeading.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const HeroHeading = ({ title }) => {
   return (
     <div className="w-[65%] lg:w-[45%] pb-12 md:pb-20 lg:pb-32 pt-24 lg:pt-40 mx-auto text-left text-white relative">
-      <h1 className="text-xl font-bold">{title}</h1>
+      <h1 className="text-xl md:text-[1.6rem] lg:text-[2rem] font-bold">
+        {title}
+      </h1>
     </div>
   );
 };
